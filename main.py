3264cfe1d2c99f06a75d3d10ce89db9d3769ceee
Mikkel Berg <devjetz>
2024-09-24 19:42:40 +0200

    player

--- main.py
+++ main.py
@@ -15,6 +15,40 @@ PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+
+class player(pygame.sprite.Sprite):
+    COLOR = (0,200,255)
+    
+    def __init__(self,x,y,width,height):
+        self.rect = pygame.Rect(x,y,width,height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = 'left'
+        self.animation_count = 0
+
+    def move(self,dx,dy):
+        self.rect.x+=dx
+        self.rect.y+=dy
+        
+    def moveleft(self,vel):
+        self.x_vel= -vel
+        if self.direction != 'left':
+            self.direction = 'left'
+            self.animation_count = 0
+
+    def moveright(self,vel):
+        self.x_vel = vel
+        if self.direction != 'right':
+            self.direction = 'right'
+            self.animation_count = 0
+
+    def loop(self,fps):
+        self.move(self.x_vel,self.y_vel)
+
+    def draw(self,win):
+        pygame.draw.rect(win,self.COLOR,self.rect)
+
 def main(window):
     clock = pygame.time.clock()
 
