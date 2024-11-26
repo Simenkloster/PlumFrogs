fee08ef5a39fc8e99d1e00178c3ca8d69a7c66f8
Simenkloster <skj1@live.no>
2024-11-26 19:50:00 +0100

    fikset opp i main

--- main.py
+++ main.py
@@ -34,26 +34,12 @@ def main(window):
     
     
     
-    #Making stuff that goes into the game
+    #Making player
     Player1 = Player(100, 100, 50, 50)
-    fire = Fire(100, HEIGHT - block_size - 64, 16, 32)
-    fire.on()
-    spike = Spike(600, HEIGHT-block_size-32, 16, 16)
-    fan = Fan(200, HEIGHT - block_size - 16, 24, 8)
-    fan.on()
-    falling_platforms = [Falling_platform(80*i, 350, 32, 10) for i in range(8, 20)]
-    trampoline = Trampoline(300, HEIGHT - block_size - 56, 28, 28)
-    #Liste med blocks som danner gulvet
-    floor = [Block(i * block_size, HEIGHT - block_size, block_size) for i in range(-WIDTH // block_size, WIDTH * 2 // block_size)]
-    #Liste med alle tingene som inngår i spillet
 
-    #Spikehead
-    spikehead = Spikehead(-100, 600, 54,  52)
 
-    objects = [*floor, fire, *falling_platforms, spike, fan, trampoline, spikehead]
-   
-   
-   
+    #Importing level
+    from mi_level import objects, loopable
 
 
     offset_x = 0
@@ -66,15 +52,12 @@ def main(window):
     while run:
         clock.tick(FPS)
         Player1.loop(FPS)
-        fire.loop()
-        fan.loop()
-        spikehead.loop()
+
         
-        for platform in falling_platforms:
-            platform.loop()
+        for obj in loopable:
+            obj.loop()
         
         
-        trampoline.loop()
         #Håndterer bevegelse og kollisjon
         handle_move(Player1, objects, PLAYER_VEL) 
         
