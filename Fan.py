76b6ed941ff5260d7240637b63e4001c75a09997
Gran134 <sindresbrekke@gmail.com>
2025-01-07 19:44:29 +0100

    la til noe

--- Fan.py
+++ Fan.py
@@ -24,7 +24,7 @@ class Fan(Object): # 24 x 8
         self.image = sprites[sprite_index]
         self.animation_count += 1
 
-        self.rect =self.image.get_rect(topleft=(self.rect.x, self.rect.y))
+        self.rect = self.image.get_rect(topleft=(self.rect.x, self.rect.y))
         self.mask = pygame.mask.from_surface(self.image)
 
         if self.animation_count // self.ANIMATION_DELAY > len(sprites):
